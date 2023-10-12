d530f300ba4caac172f30c1cccf36969db7238f9
Lauri Laaksonen <laaksonel@gmail.com>
2023-10-12 20:59:32 +0300

    MobileDatePicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MobileDatePicker } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
+import { Dialog } from '@mui/material';
+import { useState } from 'react';
+import { Button } from '@mui/base';
 
 function App() {
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<dayjs.Dayjs|null>(dayjs());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={() => setOpen(true)}>Open</Button>
+      <p>{value?.toISOString()}</p>
+      <MobileDatePicker
+        label="Controlled picker"
+        value={value}
+        open={open}
+        onClose={() => { setOpen(false); } }
+
+        onChange={(newValue) => console.log("Change", newValue)}
+        onAccept={(newValue) => {
+          setValue(newValue);
+          setOpen(false);
+        }}
+        slots={{
+          dialog: (props) => {
+            return <Dialog
+              {...props}
+
+              onClose = {(_, reason) => {
+                if (reason == "backdropClick") {
+                  setOpen(false)
+                }
+              }}
+            />
+          }
+        }}
+      />
     </div>
   );
 }
