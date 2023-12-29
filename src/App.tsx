7a2b5fbb17bc826b199d7873a0ed6add127f40fa
Lauri Laaksonen <laaksonel@gmail.com>
2023-12-29 20:19:27 +0200

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -7,43 +7,55 @@ import { useState } from 'react';
 import { Button } from '@mui/base';
 
 function App() {
-
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState<dayjs.Dayjs|null>(dayjs());
-
-  const x : any = "foo" as any;
+  const [value, setValue] = useState<dayjs.Dayjs | null>(null);
 
   return (
     <div className="App">
-      <Button onClick={() => setOpen(true)}>Open</Button>
+      <div>
+        <Button onClick={() => setOpen(true)}>Open</Button>
+      </div>
       <p>{value?.toISOString()}</p>
-      <MobileDatePicker
-        label="Controlled picker"
-        value={value}
-        open={open}
-        onClose={() => { setOpen(false); } }
-
-        onChange={(newValue) => console.log("Change", newValue)}
-        onAccept={(newValue) => {
-          setValue(newValue);
-          setOpen(false);
-        }}
-        slots={{
-          dialog: (props) => {
-            return <Dialog
-              {...props}
-
-              onClose = {(_, reason) => {
-                if (reason == "backdropClick") {
-                  setOpen(false)
-                }
-              }}
-            />
-          }
-        }}
-      />
+      <div>
+        <PrivateRoute value={value} setValue={setValue} open={open} setOpen={setOpen} />
+      </div>
     </div>
   );
+}
+
+const PrivateRoute = (props: { value: dayjs.Dayjs | null, setValue: (x: dayjs.Dayjs | null) => void, open: boolean, setOpen: (open: boolean) => void }) => {
+  const { open, setOpen, value, setValue } = props;
+
+  return <div>{open && <Picker value={value} setValue={setValue} open={open} setOpen={setOpen} />}</div>
+};
+
+const Picker = (props: { value: dayjs.Dayjs | null, setValue: (x: dayjs.Dayjs | null) => void, open: boolean, setOpen: (open: boolean) => void }) => {
+  const { open, setOpen, value, setValue } = props;
+  console.log(`rerender with value ${value}`)
+
+  return (<MobileDatePicker
+    onAccept={(newValue) => {
+      console.log('accept')
+      setValue(null)
+      setOpen(false);
+    }}
+    open={open}
+    defaultValue={dayjs()}
+    onClose={() => setOpen(false)}
+    slots={{
+      dialog: (props) => {
+        return <Dialog
+          {...props}
+
+          onClose={(_, reason) => {
+            if (reason == "backdropClick") {
+              setOpen(false)
+            }
+          }}
+        />
+      }
+    }}
+  />);
 }
 
 export default App;
